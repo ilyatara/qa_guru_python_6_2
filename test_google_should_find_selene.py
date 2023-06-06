013892d637c89314fc380af414ab7bad9f760d0b
Ilya Tarasov <qailyatarasov@gmail.com>
2023-06-06 17:44:47 +0600

    Pass fixture to test functions as an argument

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -5,14 +5,16 @@ from selene import be, have
 
 @pytest.fixture(scope="module")
 def maximize_browser_window():
-    browser.driver().maximize_window()
+    browser.driver.maximize_window()
+    # browser.config.window_width = 1920
+    # browser.config.window_height = 1080
 
-def test_google_search_positive():
+def test_google_search_positive(maximize_browser_window):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_search_negative():
+def test_google_search_negative(maximize_browser_window):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('blablabla').press_enter()
     browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
