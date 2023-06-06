d36186b77aabe82908b37d9bc9fd1745c16c8a14
Ilya Tarasov <qailyatarasov@gmail.com>
2023-06-06 17:26:33 +0600

    Added negative test

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -11,3 +11,8 @@ def test_google_search_positive():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_google_search_negative():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('blablabla').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
