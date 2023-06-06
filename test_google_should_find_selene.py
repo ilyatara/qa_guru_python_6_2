e5945e138d2cd8fbd1b5d5cf23b9651617f66575
Ilya Tarasov <qailyatarasov@gmail.com>
2023-06-06 17:25:34 +0600

    Rewrote positive test with PyTest fixture

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,9 +1,13 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
 
-browser.config.driver_name = 'firefox'
+@pytest.fixture(scope="module")
+def maximize_browser_window():
+    browser.driver().maximize_window()
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+def test_google_search_positive():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
